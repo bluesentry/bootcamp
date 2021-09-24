3576447d321905a6f49999e3ccc09cbd143bfd0e
Dennis <dennis@bluesentryit.com>
2021-09-24 10:55:01 -0500

    fixes vpc module

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -3,9 +3,8 @@ data "aws_availability_zones" "main" {}
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  azs                = local.availability_zones
-  cidr               = local.vpc_cidr
-  enable_s3_endpoint = true
-  name               = local.name
-  public_subnets     = local.public_subnets
+  azs            = local.availability_zones
+  cidr           = local.vpc_cidr
+  name           = local.name
+  public_subnets = local.public_subnets
 }
