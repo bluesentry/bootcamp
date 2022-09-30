3a6c09680e4d46041621cf3ffb60063869cabc9c
Will Ahlborn <will.ahlborn@bluesentryit.com>
2022-09-30 14:43:55 -0400

    improved outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,3 @@
-output "this" {
+output "candidate_credentials" {
   value = "The values for the candidates' credentials are now stored in AWS secrets in the region you selected."
 }
--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -12,14 +12,16 @@ resource "aws_secretsmanager_secret_version" "this" {
 
   secret_string = trim(jsonencode(flatten([
     for name, random_pet in local.pet_association : [
-      for username, password in aws_iam_user_login_profile.this : [
+      for username, password_values in aws_iam_user_login_profile.this : [
         for key, secret_values in aws_iam_access_key.this : [
           for k, v in tls_private_key.default : {
-            username          = username
-            pet_association   = random_pet
-            access_key_id     = secret_values.id
-            secret_access_key = secret_values.secret
-            ssh_key           = v.private_key_pem
+            a_console_signin_link = "https://bsisandbox.signin.aws.amazon.com/console"
+            b_username            = username
+            c_password            = password_values.password
+            d_pet_association     = random_pet
+            e_access_key_id       = secret_values.id
+            f_secret_access_key   = secret_values.secret
+            g_ssh_key             = v.private_key_pem
           } if k == each.key
         ] if key == each.key
       ] if username == each.key
